93d619f03ae1fb7b5b67b6b80ad866b3e2b58c48
AngelJohank <74012324+AngelJohank@users.noreply.github.com>
2021-07-08 08:49:14 -0500

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,178 @@
+const db = firebase.firestore(); // Database
+const loginModal = new bootstrap.Modal(document.getElementById('login-modal')); // Modal
+const loginForm = document.querySelector('#login-form'); // Login Form
+const nav = document.getElementById('navTog') // Navbar control
+var contentLoaded = false;
+
+// Firebase
+
+const onGetReg = (callback) => db.collection('registros').onSnapshot(callback); // Get data and Changes;
+const deleteReg = (id) => db.collection("registros").doc(id).delete(); // Delete Reg
+const regContainer = document.getElementById('reg-container'); // Reg-Container
+
+
+window.addEventListener('DOMContentLoaded', async(e) => {
+
+	auth.onAuthStateChanged(function (user) { // check user's status
+
+		if (user) {
+
+			console.log('Logued');
+
+			if (contentLoaded) {
+				console.log('NOPE');
+			} else {
+				console.log('Entre mi rey');
+				adminConstructor();
+			}
+
+		} else {
+
+			console.log('not logued');
+
+			loginForm.addEventListener('submit', async(e) => {
+
+				e.preventDefault();
+
+				login(); // Login
+
+			})
+
+		}
+	});
+});
+
+
+// -------------------------------  Login  ------------------------------------------------
+
+
+function login() {
+
+	const user = loginForm['login-email'].value; // Get email
+	const password = loginForm['login-password'].value; // Get password
+
+	auth
+		.signInWithEmailAndPassword(user, password)
+		.then(userCredential => {
+			contentLoaded = true;
+			adminConstructor();
+			loginModal.hide();
+		});
+
+	loginForm.reset(); // reset form
+	;
+
+}
+
+
+// -------------------------------  ADMIN PANEL  ------------------------------------------------
+
+
+//  Functions  //
+
+function adminConstructor() {
+
+	document.title = 'Administrador';
+
+	nav.innerHTML = '';
+	nav.innerHTML = `
+		<ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+            	<a class="nav-link" id="logout" href="#">Salir</a>
+            </li>
+        </ul>`;
+
+	logout(); // Configure Logout function
+
+	// Delete crap html
+
+	document.getElementById('carouselSlide').remove();
+	document.getElementById('nosotros').remove();
+	document.getElementById('productos').remove();
+	document.getElementById('ubicanos').remove();
+	document.getElementById('login-modal').remove();
+
+	// add reg fields
+
+	getRegs()
+}
+
+
+function logout() {
+	const logout = document.querySelector("#logout"); // Select logout button
+
+	logout.addEventListener("click", (e) => { // Add click event
+
+		e.preventDefault();
+
+		auth // sign out
+			.signOut()
+			.then(() => {
+				console.log('Signed out');
+				location.reload();
+			});
+
+	});
+}
+
+function getRegs() {
+
+	onGetReg((querySnapshot) => { // Fill regContainer
+
+		regContainer.innerHTML = '<h1 class="text-center" style="margin: 50px;">Administrar pedidos</h1>'; // Add a title
+
+		querySnapshot.forEach(doc => { // Generate an HTML card
+
+			const reg = doc.data('registros'); // Reg values
+			reg.id = doc.id; // Reg ID
+
+			Card(reg); // Inner Reg-cards
+			btnsEvent(); // Btns events
+
+		});
+	});
+}
+
+function Card(reg) {
+
+	regContainer.innerHTML += `
+		<div class="card card-reg">
+			<div class="card-header">
+			    Nombre: ${reg.name}
+			</div>
+			<div class="card-body">
+			    <h5 class="card-title">Datos</h5>
+			    <p class="card-text">
+		    		whatsapp: ${reg.phone} <br>
+					correo: <a href="mailto:${reg.mail}" target="_blank">${reg.mail}</a> <br>
+					tipo: ${reg.type} <br>
+					cantidad: ${reg.amount} <br>
+					Para el: ${reg.date} <br>
+					descripción: ${reg.description} <br>
+				</p>
+		    <button class="btn btn-primary btn-delete bg-danger" data-id="${reg.id}">Eliminar</button>
+		    <button class="btn btn-primary btn-contact bg-success" data-id="${reg.phone}">Contactar</button>
+			</div>
+		</div>`;
+}
+
+function btnsEvent() {
+
+	const btnsDelete = regContainer.querySelectorAll(".btn-delete");
+
+	btnsDelete.forEach(btn => {
+
+		btn.addEventListener('click', async(e) => {
+			await deleteReg(e.target.dataset.id) // Delete registry with data-id="${reg.id}"
+		});
+	});
+
+	const btnsContact = regContainer.querySelectorAll(".btn-contact");
+
+	btnsContact.forEach(btn => {
+
+		btn.addEventListener('click', async(e) => {
+			window.open(('https://api.whatsapp.com/send?phone=51' + e.target.dataset.id), '_blank'); // Open whatsapp url with phone
+		})
+	});
+}
